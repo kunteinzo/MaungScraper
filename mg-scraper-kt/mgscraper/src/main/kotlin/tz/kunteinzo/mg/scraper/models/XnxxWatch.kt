c3d2969502d6e8780fc2d6e856c557ff19294d6a
ktzo <theinhtetoo473@gmail.com>
2026-01-20 11:57:20 +0000

    add details

--- mg-scraper-kt/mgscraper/src/main/kotlin/tz/kunteinzo/mg/scraper/models/XnxxWatch.kt
+++ mg-scraper-kt/mgscraper/src/main/kotlin/tz/kunteinzo/mg/scraper/models/XnxxWatch.kt
@@ -2,4 +2,15 @@ package tz.kunteinzo.mg.scraper.models
 
 data class XnxxWatch(
     var title: String = "No Title"
+    var description: String = "",
+    var makerName: String = "",
+    var makerLink: String = "",
+    var hls: String = "",
+    var thumb: String = "",
+    var thumb169: String = "",
+    var slide: String = "",
+    var slideBig: String = "",
+    var slideMinute: String = "",
+    var mzl: String = "",
+    var pvv: String = ""
 )
